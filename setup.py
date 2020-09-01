a8dc9affd0aecc53d0dd25cf7dc471d154739ab7
stuartasims <stuart.a.sims@gmail.com>
2020-09-01 09:19:04 -0600

    Upload to Public-facing Repo
    For rendering of plotly figures with nbviewer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='contains data, results, figures, etc for motley fool data analyst assessment',
+    author='stuart sims',
+    license='',
+)
